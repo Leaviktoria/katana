49b4c393bf77543cd3f3a6208c40c7e221cc0abc
hansdav <hdavidson21@gmail.com>
2023-01-14 12:08:54 +0100

    code for showing snippet

--- memberscorePage.js
+++ memberscorePage.js
@@ -1,5 +1,38 @@
 const cheatButton = document.querySelector('#cheat');
 const finalScore = document.querySelector('#finalscore');
+const vikiDiv = document.querySelector('#viki');
+const leaDiv = document.querySelector('#lea');
+const hansDiv = document.querySelector('#hans');
+const vikiChildNodes = document.getElementById('viki').childNodes;
+const leaChildNodes = document.getElementById('lea').childNodes;
+
+const showVikiSnippet = () => {
+	for (let i = 0; i < vikiChildNodes.length; i++) {
+		if (vikiChildNodes[i].nodeName.toLowerCase() == 'div') {
+			vikiChildNodes[i].style.display = 'none';
+		}
+	}
+	let vikiSnippet = document.createElement('img');
+	vikiSnippet.style.visibility = 'visible';
+	vikiSnippet.src = './viki-snippet.png';
+	vikiSnippet.style.width = '100%';
+	vikiSnippet.style.height = '100%';
+	vikiDiv.appendChild(vikiSnippet);
+};
+
+const showVikiSnippet = () => {
+	for (let i = 0; i < vikiChildNodes.length; i++) {
+		if (vikiChildNodes[i].nodeName.toLowerCase() == 'div') {
+			vikiChildNodes[i].style.display = 'none';
+		}
+	}
+	let vikiSnippet = document.createElement('img');
+	vikiSnippet.style.visibility = 'visible';
+	vikiSnippet.src = './viki-snippet.png';
+	vikiSnippet.style.width = '100%';
+	vikiSnippet.style.height = '100%';
+	vikiDiv.appendChild(vikiSnippet);
+};
 
 const scoreCounter = () => {
 	finalScore.innerHTML++;
@@ -8,3 +41,5 @@ const scoreCounter = () => {
 };
 
 cheatButton.addEventListener('click', scoreCounter);
+
+vikiDiv.addEventListener('click', showSnippet);
